50145d986680ff7e4379be60db809e352aa77788
okruhlia <okruhlia@student.42warsaw.pl>
2025-07-11 12:26:59 +0200

    C05

--- C04/ex04/ft_putnbr_base.c
+++ C04/ex04/ft_putnbr_base.c
@@ -6,36 +6,39 @@
 /*   By: okruhlia <okruhlia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 18:03:05 by okruhlia          #+#    #+#             */
-/*   Updated: 2025/07/10 19:18:05 by okruhlia         ###   ########.fr       */
+/*   Updated: 2025/07/11 11:31:54 by okruhlia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
 int	base_verifi(char *base);
+int	is_duble(char *base);
 
 void	ft_putnbr_base(int nbr, char *base)
 {
-	int		devisor;
-	char	cash[100];
-	int		id;
-	int		sign;
+	int			devisor;
+	char		cash[100];
+	int			id;
+	long int	sign;
 
 	devisor = base_verifi(base);
-	if (devisor == 0)
+	if (devisor == 0 || devisor == 1)
 		write(1, "", 1);
+	else if (nbr == 0)
+		write(1, &base[0], 1);
 	else
 	{
 		id = 0;
 		sign = nbr;
-		if (nbr < 0)
-			nbr *= -1;
-		while (nbr > 0)
-		{
-			cash[id++] = base[(nbr % devisor)];
-			nbr /= devisor;
-		}
 		if (sign < 0)
+			sign *= -1;
+		while (sign > 0)
+		{
+			cash[id++] = base[(sign % devisor)];
+			sign /= devisor;
+		}
+		if (nbr < 0)
 			cash[id] = '-';
 		while (id >= 0)
 			write(1, &cash[id--], 1);
@@ -44,14 +47,40 @@ void	ft_putnbr_base(int nbr, char *base)
 
 int	base_verifi(char *base)
 {
-	if (base == "0123456789")
-		return (10);
-	else if (base == "01")
-		return (2);
-	else if (base == "0123456789ABSDEF")
-		return (16);
-	else if (base == "poneyvif")
-		return (8);
-	else
+	int	id;
+
+	id = 0;
+	if (!base[0])
 		return (0);
+	if (is_duble(base) == 1)
+		return (0);
+	while (base[id] != '\0')
+	{
+		if (!(('a' <= base[id] && base[id] <= 'z')
+				|| ('A' <= base[id] && base[id] <= 'Z')
+				|| ('0' <= base[id] && base[id] <= '9')))
+			return (0);
+		id++;
+	}
+	return (id);
+}
+
+int	is_duble(char *base)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (base[i] != '\0')
+	{
+		j = i + 1;
+		while (base[j] != '\0')
+		{
+			if (base[i] == base[j])
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
 }
