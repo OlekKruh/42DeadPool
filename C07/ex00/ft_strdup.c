0b318fbf6eb26f9f0c70cc69e6f5f75133c01bee
okruhlia <okruhlia@student.42warsaw.pl>
2025-07-14 15:48:36 +0200

    add C07 ex00-ex01

---
+++ C07/ex00/ft_strdup.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okruhlia <okruhlia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/14 15:20:18 by okruhlia          #+#    #+#             */
+/*   Updated: 2025/07/14 15:40:35 by okruhlia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(char *src);
+
+char	*ft_strdup(char *src)
+{
+	char	*new_str;
+	int		id;
+
+	id = 0;
+	new_str = (char *)malloc(ft_strlen(src) + 1);
+	if (new_str == NULL)
+		return (0);
+	else
+	{
+		while (src[id] != '\0')
+		{
+			new_str[id] = src[id];
+			id++;
+		}
+	}
+	new_str[id] = '\0';
+	return (new_str);
+}
+
+int	ft_strlen(char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+		i++;
+	return (i);
+}
