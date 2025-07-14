a7ad60c359b9074dde3d1882ff88bc5548a4d1db
okruhlia <okruhlia@student.42warsaw.pl>
2025-07-14 11:08:22 +0200

    C05 ex07

--- C05/ex07/ft_find_next_prime.c
+++ C05/ex07/ft_find_next_prime.c
@@ -6,36 +6,22 @@
 /*   By: okruhlia <okruhlia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 16:07:55 by okruhlia          #+#    #+#             */
-/*   Updated: 2025/07/13 17:17:03 by okruhlia         ###   ########.fr       */
+/*   Updated: 2025/07/14 11:07:27 by okruhlia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_find_next_prime(int nb)
 {
-	int 	i;
+	int	i;
 
 	i = 0;
-	if (nb <= 1)
+	if (nb < 0 || nb == 0 || nb == 1)
 		return (2);
-	else
+	if (nb == 2)
+		return (3);
+	while (((nb + i) % 2 == 0) || ((nb + i) % 3 == 0))
 	{
-		while(1)
-		if (nb + i == nb)
+		i++;
 	}
-	// int	i;
-
-	// i = 0;
-	// while (nb + i >= nb)
-	// {
-	// 	if (nb == 0 || nb == 1)
-	// 		return (2);
-	// 	if (nb == 2 || nb == 3)
-	// 		return (nb);
-	// 	if ((nb % 2 == 0) || (nb % 3 == 0))
-	// 	{
-	// 		i++;
-	// 	}
-	// 	return (nb + i);
-	// }
-	// return (404);
+	return (nb + i);
 }
