388448f87c7bb578459b5a41d8ef90a0db51b798
OlekKruh <ksalexks@gmail.com>
2025-07-15 23:14:16 +0200

    poprawka C05 ex06-ex07

--- C05/ex06/ft_is_prime.c
+++ C05/ex06/ft_is_prime.c
@@ -17,9 +17,10 @@ int	ft_is_prime(int nb)
 	int	i;
 
 	i = 2;
-	if (nb <= 0 || nb == 1)
+	if (nb <= 1)
 		return (0);
-
+	if (nb == 2);
+		return (1);
 	while ((i * i) <= nb)
 	{
 		if (nb % i == 0)
